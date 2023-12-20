bf1a65711fa4044bd9c3e0bf9c8843fe2f8acf16
Akansha Rai <akansharai601@gmail.com>
2023-12-20 13:20:32 +0530

    icos added

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+  SpecialOffer,
+  SuperQuality,
+  Services,
+  PopularProducts,
+};
